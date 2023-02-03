c576b97d11e7a7dea3cd0177413698536f1a144a
Shariq Shaikh <sshaikh524@gmail.com>
2023-02-03 11:14:30 -0500

    Some more py4e stuff

---
+++ py4e_assignments/py4eAssignment3.py
@@ -0,0 +1,19 @@
+# 2.2 Write a program that uses input to prompt a user for their name and then welcomes them. Note that input will
+# pop up a dialog box. Enter Sarah in the pop-up box when you are prompted so your output will match the desired
+# output. The code below almost works
+
+name = input("Enter your name")
+print(f"Hello {name}")
+
+# 2.3 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours
+# and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string
+# and float() to convert the string to a number. Do not worry about error checking or bad user data.
+
+# This first line is provided for you
+
+hrs = input("Enter Hours: ")
+pay_rate = input('Enter Pay Rate Per Hour (please enter only integers: ')
+final_pay = float(pay_rate) * float(hrs)
+print(f'Pay: {final_pay}')
+
+

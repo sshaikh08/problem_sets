c99c15ed491f34d3909aa866a32cbf47c623428c
Shariq Shaikh <sshaikh524@gmail.com>
2023-02-14 19:13:16 -0500

    Some changes to assignment 3.2

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,10 @@
-print(int(98.6))
+
+    try:
+        score = float(input("Enter Score between 0 and 1: "))
+        if 0 <= score <= 1:
+        else:
+            raise ValueError
+    except ValueError:
+        print('Invalid value input, value must be a decimal value between 0 and 1')
+
+print(score)

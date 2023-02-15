c981391a4e28c55c50e47278f41e30fa1b422223
sharkshake <sshake08@gmail.com>
2023-02-15 09:14:33 -0500

    Did a bunch of reorganizing for py4e

--- py4e_assignments/assignment4/py4eAssignment3.1.py
+++ py4e_assignments/assignment4/py4eAssignment3.1.py
@@ -15,16 +15,4 @@ if hrs > 40:
     over_time_pay = pay_rate * 1.5 * over_time_hours
 
 final_pay = (regular_hours*pay_rate) + over_time_pay
-print(f'Pay: {final_pay}')
 
-# 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the
-# score is between 0.0 and 1.0, print a grade using the following table:
-# Score Grade
-# >= 0.9 A
-# >= 0.8 B
-# >= 0.7 C
-# >= 0.6 D
-# < 0.6 F
-# If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
-
-score = float(input("Enter Score: "))

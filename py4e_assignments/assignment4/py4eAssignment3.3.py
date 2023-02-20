1451dd2b79d2715f9acd70624b210f6202a52145
Shariq Shaikh <sshaikh524@gmail.com>
2023-02-20 18:47:09 -0500

    02/20/23 push

--- py4e_assignments/assignment4/py4eAssignment3.3.py
+++ py4e_assignments/assignment4/py4eAssignment3.3.py
@@ -4,4 +4,46 @@
 # 0.9 A >= 0.8 B >= 0.7 C >= 0.6 D < 0.6 F If the user enters a value out of range, print a suitable error message
 # and exit. For the test, enter a score of 0.85.
 
-score = float(input("Enter Score: "))
+# My Submission
+try:
+    Grade = float(input("Enter Grade between 0 and 1: "))
+    if not 0 <= Grade <= 1:
+        raise ValueError
+    if Grade >= 0.9:
+        print('A')
+    elif 0.9 > Grade >= 0.8:
+        print('B')
+    elif 0.8 > Grade >= 0.7:
+        print('C')
+    elif 0.7 > Grade >= 0.6:
+        print('D')
+    elif 0.6 > Grade:
+        print('F')
+except ValueError:
+    print('Invalid value input, value must be a decimal value between 0 and 1')
+
+############
+# Optimized Solution
+
+try:
+    grade_str = input("Enter grade between 0 and 1: ")
+    grade = float(grade_str)
+    if not 0 <= grade <= 1:
+        raise ValueError
+    if grade >= 0.9:
+        print('A')
+    elif grade >= 0.8:
+        print('B')
+    elif grade >= 0.7:
+        print('C')
+    elif grade >= 0.6:
+        print('D')
+    else:
+        print('F')
+except ValueError:
+    print('Invalid value input, value must be a decimal value between 0 and 1')
+
+
+
+
+

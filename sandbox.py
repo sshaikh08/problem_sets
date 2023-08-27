a23f73fc26c211039e1184adfe375615bd868c3d
Shariq Shaikh <sshaikh524@gmail.com>
2023-08-27 18:16:30 -0400

    sandbox changes

--- sandbox.py
+++ sandbox.py
@@ -1,12 +1,6 @@
-import re
-with open('mbox-short.txt') as hand:
-    numlist = list()
-    for line in hand:
-        line = line.rstrip()
-        stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
-        if len(stuff) != 1 : continue
-        num = float(stuff[0])
-        numlist.append(num)
-print('Maximum:', max(numlist))
+# The code below almost works
+
+name = input("Enter your name")
+print("Howdy")
 
 
